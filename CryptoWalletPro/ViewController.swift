1a31caa2cc1b9d50ed6983dd83fc2e2746789695
GonyaDev <gonyadev@yandex.ru>
2022-07-27 16:23:26 +0300

    Initial Commit

---
+++ CryptoWalletPro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoWalletPro
+//
+//  Created by Алексей Гончаров on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
